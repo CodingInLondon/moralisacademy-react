6eb4aa5f397bbba752dbacf15fc0035b6f3aa0f2
CodingInLondon <github@golios.com>
2023-03-22 10:53:04 +0000

    Added styled components for balance

--- coin-exchange/src/Components/AccountBalance/AccountBalance.jsx
+++ coin-exchange/src/Components/AccountBalance/AccountBalance.jsx
@@ -1,15 +1,27 @@
 
 import React, { Component } from 'react';
-import './AccountBalance.css'
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Section = styled.section`
+    padding: 1.5rem 0 1.5rem 4rem;
+    color: grey;
+    font-weight: bold;
+    color: #333; 
+    text-align: left;
+    border-radius: 10px;
+    background-color: #f5f5f5;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
  
 export default class AccountBalance extends Component{
 
     render(){
         return(
-            <section className='balance'>
+            <Section>
                 Balance: ${this.props.amount}
-            </section>
+            </Section>
         );
     }
 
