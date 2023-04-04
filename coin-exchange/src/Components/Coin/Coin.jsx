de7e8167c034ad96d95af1c702ed19b074d040c8
CodingInLondon <github@golios.com>
2023-04-04 16:55:42 +0100

    Re-styled the table

--- coin-exchange/src/Components/Coin/Coin.jsx
+++ coin-exchange/src/Components/Coin/Coin.jsx
@@ -3,47 +3,56 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+
+
 const Tr = styled.tr`
-border: 1px solid #ccc;
-background-color: #f7f7f7;
-color: #333;
-font-size: 14px;
-font-weight: 500;
-text-align: center;
-transition: all 0.3s ease-in-out;
+  border: 1px solid #ccc;
+  background-color: #f7f7f7;
+  color: #333;
+  font-size: 14px;
+  font-weight: 500;
+  text-align: center;
+  transition: all 0.3s ease-in-out;
 
+  &:nth-child(odd) {
+    background-color: #e9f8ff;
+  }
 
-&:hover {
-  background-color: #e7e7e7;
-  border-color: #aaa;
-}
+  &:hover {
+    background-color: #d6e8ff;
+    border-color: #aaa;
+  }
 
-td:first-child {
-  text-align: left;
-  padding-left: 10px;
-}
+  td:first-child {
+    text-align: left;
+    padding-left: 10px;
+  }
 
-td:last-child {
-  padding-right: 10px;
+  td:nth-child(3) {
+    text-align: right;
+  }
 
-  button {
-    background-color: #6c757d;
-    color: #fff;
-    border: none;
-    border-radius: 4px;
-    padding: 6px 12px;
-    font-size: 14px;
-    font-weight: 500;
-    cursor: pointer;
-    transition: all 0.3s ease-in-out;
+  td:last-child {
+    padding-right: 10px;
 
-    &:hover {
-      background-color: #555;
+    button {
+      background-color: #6c757d;
+      color: #fff;
+      border: none;
+      border-radius: 4px;
+      padding: 6px 12px;
+      font-size: 14px;
+      font-weight: 500;
+      cursor: pointer;
+      transition: all 0.3s ease-in-out;
+
+      &:hover {
+        background-color: #555;
+      }
     }
   }
-}
-
 `;
+
 
 export default class Coin extends Component {
 
